551e9943b1f9253f1ea1580eef4117e673939f16
KelvinCosta <kelvinvicentecosta@gmail.com>
2020-07-13 10:35:38 -0300

    Inicio do projeto, teste canário funcionando

---
+++ demo/src/test/java/edu/movdb/demo/DemoApplicationTests.java
@@ -0,0 +1,16 @@
+package edu.movdb.demo;
+
+import static org.junit.Assert.assertTrue;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.Test;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void canary() {
+		assertTrue(true);
+	}
+
+}
