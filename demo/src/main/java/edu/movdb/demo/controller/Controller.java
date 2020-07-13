90f9460f7a2c691fd79dff19c2131c0a22d9a6db
KelvinCosta <kelvinvicentecosta@gmail.com>
2020-07-13 17:03:48 -0300

    corrigido teste de fumaça para chamar o mockmvc;teste de status de retorno passando

--- demo/src/main/java/edu/movdb/demo/controller/Controller.java
+++ demo/src/main/java/edu/movdb/demo/controller/Controller.java
@@ -1,8 +1,16 @@
 package edu.movdb.demo.controller;
 
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @RestController
 public class Controller {
-  
+
+  @GetMapping("/filmes/{movieName}/{pageNumber}")
+  public ResponseEntity movieInfo(){
+    return new ResponseEntity(HttpStatus.OK);
+  }
+
 }
--- demo/src/test/java/edu/movdb/demo/controller/Controller.java
+++ demo/src/test/java/edu/movdb/demo/controller/Controller.java
@@ -2,16 +2,20 @@ package edu.movdb.demo.controller;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.web.servlet.MockMvc;
 
-@SpringBootTest
+@WebMvcTest
 class ControllerTests {
 
   @Autowired
-  Controller controller;
+  MockMvc mockMvc;
 
   @Test
   void canary(){
@@ -20,7 +24,12 @@ class ControllerTests {
 
   @Test
   void smokeTest(){
-    assertNotNull(controller);
+    assertNotNull(mockMvc);
+  }
+
+  @Test
+  void movieInfo() throws Exception {
+    this.mockMvc.perform(get("/filmes/waterworld/1")).andExpect(status().isOk());
   }
   
 }
