08743067d05672b933614fc18f57ffafff39f18c
KelvinCosta <kelvinvicentecosta@gmail.com>
2020-07-13 12:46:19 -0300

    Adicionado teste de fumaça para classe Controller

--- demo/src/test/java/edu/movdb/demo/controller/Controller.java
+++ demo/src/test/java/edu/movdb/demo/controller/Controller.java
@@ -1,16 +1,26 @@
 package edu.movdb.demo.controller;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import org.springframework.boot.test.context.SpringBootTest;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @SpringBootTest
 class ControllerTests {
 
+  @Autowired
+  Controller controller;
+
   @Test
   void canary(){
     assertTrue(true);
+  }
+
+  @Test
+  void smokeTest(){
+    assertNotNull(controller);
   }
   
 }
