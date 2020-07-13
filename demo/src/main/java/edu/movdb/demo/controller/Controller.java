b3253a6e4fd5c0c10a110faa8e9e2bc4d2bd5783
KelvinCosta <kelvinvicentecosta@gmail.com>
2020-07-13 17:51:06 -0300

    teste de retorno de mediatype passando; 2 de 2 testes verdes

--- demo/src/main/java/edu/movdb/demo/controller/Controller.java
+++ demo/src/main/java/edu/movdb/demo/controller/Controller.java
@@ -4,12 +4,18 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
 public class Controller {
 
-  @GetMapping("/filmes/{movieName}/{pageNumber}")
-  public ResponseEntity movieInfo(){
+  @RequestMapping(
+    path = "/filmes/{movieName}/{pageNumber}", 
+    produces = "application/json; charset = UTF-8")
+  public ResponseEntity movieInfo(
+    @PathVariable("movieName")  String movieName,
+    @PathVariable("pageNumber") String pageNumber){
     return new ResponseEntity(HttpStatus.OK);
   }
 
