41f9dfd8ed00dd12fdbf6416ac9b54d20e7bf90d
KelvinCosta <kelvinvicentecosta@gmail.com>
2020-07-14 10:39:22 -0300

    Teste e implementação que acabaram não sendo salvos

--- demo/src/main/java/edu/movdb/demo/controller/Controller.java
+++ demo/src/main/java/edu/movdb/demo/controller/Controller.java
@@ -3,6 +3,7 @@ package edu.movdb.demo.controller;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpHeaders;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,10 +14,12 @@ public class Controller {
   @RequestMapping(
     path = "/filmes/{movieName}/{pageNumber}", 
     produces = "application/json; charset = UTF-8")
-  public ResponseEntity movieInfo(
+  public ResponseEntity<String> movieInfo(
     @PathVariable("movieName")  String movieName,
     @PathVariable("pageNumber") String pageNumber){
-    return new ResponseEntity(HttpStatus.OK);
+    HttpHeaders responseHeaders = new HttpHeaders();
+    responseHeaders.set("content-type", "application/json; charset=utf-8");
+    return new ResponseEntity<String>("", responseHeaders, HttpStatus.OK);
   }
 
 }
--- demo/src/test/java/edu/movdb/demo/controller/Controller.java
+++ demo/src/test/java/edu/movdb/demo/controller/Controller.java
@@ -3,13 +3,15 @@ package edu.movdb.demo.controller;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.*;
 
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.http.MediaType;
 
 @WebMvcTest
 class ControllerTests {
@@ -28,8 +30,13 @@ class ControllerTests {
   }
 
   @Test
-  void movieInfo() throws Exception {
+  void movieInfoStatusOk() throws Exception {
     this.mockMvc.perform(get("/filmes/waterworld/1")).andExpect(status().isOk());
+  }
+
+  @Test
+  void movieInfoIsJson() throws Exception {
+    this.mockMvc.perform(get("/filmes/waterworld/1")).andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8));
   }
   
 }
