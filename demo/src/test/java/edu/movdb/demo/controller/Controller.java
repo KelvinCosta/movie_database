3386e8d4c3942b011599d7a12719de15ae054e60
KelvinCosta <kelvinvicentecosta@gmail.com>
2020-07-20 18:22:35 -0300

    Teste canário verde

--- demo/src/test/java/edu/movdb/demo/controller/Controller.java
+++ demo/src/test/java/edu/movdb/demo/controller/Controller.java
@@ -1,4 +1,20 @@
 package edu.movdb.demo.controller;
 
+import static org.junit.Assert.assertTrue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+
+@WebMvcTest(controllers = Controller.class)
 class ControllerTests {  
+
+  @MockBean
+  Controller controller;
+
+  @Test
+  void canary(){
+    assertTrue(true);
+  }
+
 }
